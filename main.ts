9f488909ab1fe4a7ddb704bcd2bad16b782267f9
mrsamuelben <101801986+mrsamuelben@users.noreply.github.com>
2022-04-25 13:39:12 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 let playerCar: game.LedSprite = null
 let score = 0
 let gameOn = false
+let car0: game.LedSprite = null
 input.onButtonPressed(Button.A, function () {
     if (playerCar.get(LedSpriteProperty.X) > 0) {
         playerCar.change(LedSpriteProperty.X, -1)
@@ -20,4 +21,34 @@ basic.forever(function () {
     }
     score += score
     game.gameOver()
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    basic.pause(100)
+    if (gameOn == true) {
+        car0 = game.createSprite(0, 0)
+        basic.pause(randint(0, 5000))
+        while (gameOn == true) {
+            if (car0.get(LedSpriteProperty.Y) == 4) {
+                if (playerCar.isTouching(car0)) {
+                    gameOn = false
+                } else {
+                    score = score + 1
+                    car0.set(LedSpriteProperty.Y, 0)
+                    basic.pause(randint(0, 5000))
+                }
+            } else {
+                car0.change(LedSpriteProperty.Y, 1)
+                basic.pause(500)
+            }
+        }
+    }
 })
